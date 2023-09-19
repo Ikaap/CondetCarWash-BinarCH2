246925c5b17a8becb60fe48031d889d126309cc2
Ikap <ikapurwanti144@gmail.com>
2023-09-19 20:44:58 +0700

    finishing project
    finishing project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CondetCarWash_ch2"
+include(":app")
+ 
